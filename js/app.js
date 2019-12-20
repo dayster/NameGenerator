f4b1e14397522470f991824b872a0928b69aa5c1
Phyo Wai Aung <phyowaiaung@yomabank.com>
2019-12-20 12:03:36 +0630

    Refactor & Ajax Call To Fetch Api Using Arrow Function

--- js/app.js
+++ js/app.js
@@ -20,31 +20,49 @@ function loadNames(e){
     if(amount !== ''){
         url += `amount=${amount}`;
     }
-    console.log(`Url ${url}`);
 
-    // Ajax Call
-    const xhr = new XMLHttpRequest();
+    // Fetch Api
 
-    // Open the connection
-    xhr.open('GET', url, true );
-
-    xhr.onload = function() {
-        if(this.status === 200) {
-            const names = JSON.parse( this.responseText );
-            
-            // Insert Html
+    fetch(url)
+        .then( response => response.json())
+        .then( names => {
             let html = '<h2>Generated Names</h2>';
             html += '<ul class="list">';
-            names.forEach(name => {
+            names.forEach(function(name){
                 html += `
                     <li>${name.name}</li>
-                `;
+                `
             });
             html += '</ul>';
             document.querySelector('#result').innerHTML = html;
-        }
-   }
+        })
+        .catch( error => console.log(error));
 
-   // Send the request
-   xhr.send();
+//     // Ajax Call
+//     const xhr = new XMLHttpRequest();
+
+//     // Open the connection
+//     xhr.open('GET', url, true );
+
+//     xhr.onload = function() {
+//         if(this.status === 200) {
+//             const names = JSON.parse( this.responseText );
+            
+//             // Insert Html
+//             let html = '<h2>Generated Names</h2>';
+//             html += '<ul class="list">';
+//             names.forEach(name => {
+//                 html += `
+//                     <li>${name.name}</li>
+//                 `;
+//             });
+//             html += '</ul>';
+//             document.querySelector('#result').innerHTML = html;
+//         }
+//    }
+
+//    // Send the request
+//    xhr.send();
+
+
 }
